dac16f97a6604f8fe82cb8cf1b82239fc8abf0de
Hüseyin Çalışkan <caliskanhsyn@gmail.com>
2016-04-19 00:46:25 +0300

    Initial commit

---
+++ app/src/main/java/cepni/tekno/qrreader/CaptureActivityAnyOrientation.java
@@ -0,0 +1,9 @@
+package cepni.tekno.qrreader;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * Created by HuseyinPC on 19/04/16.
+ */
+public class CaptureActivityAnyOrientation extends CaptureActivity {
+}
---
+++ app/src/main/java/cepni/tekno/qrreader/MainActivity.java
@@ -0,0 +1,51 @@
+package cepni.tekno.qrreader;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Activity activity;
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        activity = this;
+
+        textView = (TextView)findViewById(R.id.textView_value);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setCaptureActivity(CaptureActivityAnyOrientation.class);
+                integrator.setOrientationLocked(false);
+                integrator.initiateScan();
+            }
+        });
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+
+        if ((scanResult != null) && (scanResult.getContents() != null)) {
+            String data = scanResult.getContents();
+
+            textView.setText(data);
+        } else {
+
+        }
+    }
+}
